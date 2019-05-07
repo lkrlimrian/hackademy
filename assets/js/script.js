8e22d6c8af2f30e83c991343957f17c8591def40
lkrlimrian <lkrlimrian@gmail.com>
2019-05-07 17:15:27 +0800

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-// $(document).ready(function(){
-//   $(".owl-carousel").owlCarousel({
-//       loop: true,
-//       margin: 30,
-//       dots: true
-//   });
-// });
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 30,
+      dots: true
+  });
+});
