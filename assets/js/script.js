00d4a7d87cf986c52fd82b22f0b0ccb706f269e1
lkrlimrian <lkrlimrian@gmail.com>
2019-05-08 12:41:13 +0800

    updates testimony carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,17 @@ $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
       loop: true,
       margin: 30,
-      dots: true
+      dots: true,
+      responsive:{
+        0:{
+          items: 1
+        },
+        768:{
+          items: 2
+        },
+        1360:{
+          items: 3
+        }
+      }
   });
 });
